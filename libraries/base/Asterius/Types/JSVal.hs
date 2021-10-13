74a50cba6e8f14a23a74b83ff5ec7fd76ed2b3e8
Cheng Shao <cheng.shao@tweag.io>
2021-10-13 16:11:54 +0000

    base: asterius specific logic

---
+++ libraries/base/Asterius/Types/JSVal.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE UnliftedFFITypes #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
+#if defined(ASTERIUS)
+module Asterius.Types.JSVal
+  ( JSVal (..),
+    freeJSVal,
+  )
+where
+
+import GHC.Base
+import GHC.Exts
+
+type JSVal# = StableName# ()
+
+data JSVal
+  = JSVal JSVal#
+
+instance Eq JSVal where
+  {-# INLINE (==) #-}
+  (==) = js_eqJSVal
+
+instance Ord JSVal where
+  {-# INLINE (<=) #-}
+  (<=) = js_leJSVal
+
+{-# INLINE freeJSVal #-}
+freeJSVal :: JSVal -> IO ()
+freeJSVal (JSVal sn) = js_freeJSVal sn
+
+foreign import javascript unsafe "__asterius_jsffi.freeJSValzh($1)" js_freeJSVal :: JSVal# -> IO ()
+
+foreign import javascript unsafe "$1 === $2" js_eqJSVal :: JSVal -> JSVal -> Bool
+
+foreign import javascript unsafe "$1 <= $2" js_leJSVal :: JSVal -> JSVal -> Bool
+#else
+module Asterius.Types.JSVal where
+#endif
